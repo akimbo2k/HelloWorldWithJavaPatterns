b89382d3307eefed1e0a281daaa9e94b31b27151
avalbuenae <avalbuenae@outlook.com>
2022-12-03 15:38:52 -0500

    INI

---
+++ HelloWorldAppClass.java
@@ -0,0 +1,20 @@
+/**
+ * HelloWorldAppClass
+ */
+public class HelloWorldAppClass {
+
+    public static void main(String[] args) {
+            
+        SayHello();
+
+    }
+
+    
+    /**
+     * main app says hello
+     */
+    public static void SayHello() {    
+        System.out.println("HelloWorldAppClass : Hello World!");
+    }
+    
+}
